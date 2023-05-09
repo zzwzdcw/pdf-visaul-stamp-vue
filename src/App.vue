90affeea12618778429d89bfd55119aadf3e85e2
zzwzdcw <41219791+zzwzdcw@users.noreply.github.com>
2023-05-09 09:04:40 +0800

    增加签字模拟

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
-    <button @click="change()">切换</button>
-    <PdfLocal v-if="isLocal"/>
-    <PdfView  v-else />
+    <PdfView/>
 
   </div>
 </template>
 
 <script>
 import PdfView from '@/components/PDF.vue';
-import PdfLocal from '@/components/PDFLocal.vue'
 export default {
   name: 'App',
   components: {
     PdfView,
-    PdfLocal
   },
   data() {
     return {
--- src/components/PDF.vue
+++ src/components/PDF.vue
@@ -14,6 +14,8 @@
     <button @click="clear()">清 空</button>
     <button @click="change()">切 换</button>
     <button @click="stamp()">盖 章</button>
+    <button @click="sign()">签 字</button>
+
   </div>
 </template>
 
@@ -79,24 +81,8 @@ export default {
       this.pdfList = JSON.parse(sessionStorage.getItem("pdfList"));
       this.fullCanvas();
     },
-    stamp() {
-      this.backEndPoint.vertical = this.isVertical;
-      fetch("http://localhost:9000/stamp", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(this.backEndPoint),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          //填充图片
-          this.pdfList = data.data;
-          this.fullCanvas();
-        });
-    },
-    //模拟盖章
-    stampTest(mouse) {
+    signTest(mouse){
+
       //圆的半径，根据印章的大小来配置
       let r = 50;
       //减3是因为设置了border为3px
@@ -116,6 +102,71 @@ export default {
       //因w3c坐标系和itext坐标系不同，因此需要再次转换
       this.point.y = this.height - this.point.y;
       console.log("圆的左下坐标为：" + this.point.x + "," + this.point.y);
+      this.backEndPoint.pointVoList.push({
+        x: this.point.x,
+        y: this.point.y,
+        page: this.nowPage,
+      });
+    },
+    sign(){
+      this.backEndPoint.vertical = this.isVertical;
+      fetch("http://localhost:9000/sign", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.backEndPoint),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          //填充图片
+          this.pdfList = data.data;
+          this.fullCanvas();
+        });
+    },
+    stamp() {
+      this.backEndPoint.vertical = this.isVertical;
+      fetch("http://localhost:9000/stamp", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.backEndPoint),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          //填充图片
+          this.pdfList = data.data;
+          this.fullCanvas();
+        });
+    },
+    //模拟签名
+    stampTest(mouse) {
+      //减3是因为设置了border为3px
+      let x = Number(mouse.offsetX) - 3;
+      let y = Number(mouse.offsetY) - 3;
+      //在itext中，坐标需要进行转换
+      let x1 = x - 50;
+      let x2 = x + 50;
+      let y1 = y - 15;
+      let y2 = y + 15;
+
+      this.point.x = x - 50;
+      this.point.y = y + 15;
+      //获取canvas实例
+      const cnv = this.$refs.pdfcanvas;
+      const cxt = cnv.getContext("2d");
+      //画长方形模拟签字
+      cxt.beginPath();
+      cxt.moveTo(x1,y1);
+      cxt.lineTo(x2,y1);
+      cxt.lineTo(x2,y2);
+      cxt.lineTo(x1,y2);
+      cxt.lineTo(x1,y1);
+      cxt.stroke();
+      //因w3c坐标系和itext坐标系不同，因此需要再次转换
+      this.point.y = this.height - this.point.y;
+      console.log("长方形的左下坐标为：" + this.point.x + "," + this.point.y);
       this.backEndPoint.pointVoList.push({
         x: this.point.x,
         y: this.point.y,
